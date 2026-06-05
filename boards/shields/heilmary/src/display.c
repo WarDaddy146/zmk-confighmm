da4aac7c431601b01b1cfee63e1063fc6e83d3e2
Ali <ali@example.com>
2026-06-05 13:35:41 +0530

    fix: swap black/white since display inverts pixel values

--- boards/shields/heilmary/src/display.c
+++ boards/shields/heilmary/src/display.c
@@ -5,12 +5,12 @@
 lv_obj_t *zmk_display_status_screen(void)
 {
     lv_obj_t *screen = lv_obj_create(NULL);
-    lv_obj_set_style_bg_color(screen, lv_color_black(), LV_PART_MAIN);
+    lv_obj_set_style_bg_color(screen, lv_color_white(), LV_PART_MAIN);
 
     lv_obj_t *pixel = lv_obj_create(screen);
     lv_obj_set_size(pixel, 1, 1);
     lv_obj_set_pos(pixel, 0, 0);
-    lv_obj_set_style_bg_color(pixel, lv_color_white(), LV_PART_MAIN);
+    lv_obj_set_style_bg_color(pixel, lv_color_black(), LV_PART_MAIN);
     lv_obj_set_style_bg_opa(pixel, LV_OPA_COVER, LV_PART_MAIN);
     lv_obj_set_style_border_width(pixel, 0, LV_PART_MAIN);
     lv_obj_set_style_pad_all(pixel, 0, LV_PART_MAIN);
